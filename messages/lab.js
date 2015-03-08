3ec86d1af8f0468845449a22f45260e2e64dfe93
Norman Young <nyyoung24@gmail.com>
2015-03-08 15:57:42 -0400

    debugging lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,23 +1,18 @@
-console.log("in script!");
 function parse() 
 {
-	console.log("first flag");
 	request = new XMLHttpRequest();
-	request.open("get", "data.json", true);
-	console.log("second flag");
+	request.open("GET", "data.json", true);
 	request.onreadystatechange = parseData;
 	request.send();
 }
 
 function parseData() 
 {
-	console.log("in callback " + request.readyState);
 	if (request.readyState == 4 && request.status == 200) {
-		alert("Got data back!");
-		data = JSON.parse(request.response);
+		document.getElementById("messages").innerHTML = request.responseText;
+		data = JSON.parse(request.responseText);
 		for (i = 0; i < data.length; i++) {
 			result += "<p>" + data[i]["content"] + " - " + data[i]['username'] + "</p>";
 		}
-		document.getElementById("messages").innerHTML = request.response;
 	}
 }
