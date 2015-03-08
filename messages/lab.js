1e9bc8d8ff113a2628c62ba03fb3022a04a2ee43
Norman Young <nyyoung24@gmail.com>
2015-03-07 22:11:06 -0500

    added parse and parseData functions

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
-// Your JavaScript goes here...
+
+function parse() {
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+	xhr.onreadystatechange = parseData;
+	xhr.send();
+
+function parseData() {
+	console.log("in callback " + request.readyState);
+	if (request.readyState == 4 && request.status == 200) {
+		alert("Got data back!");
+		data = JSON.parse(request.responseText);
+
+		document.getElementById("messages").innerHTML = request.responseText;
+	}
+}
