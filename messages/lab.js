a9670250549e6d70d1dfc8463fa803a476774664
Norman Young <nyyoung24@gmail.com>
2015-03-08 17:02:21 -0400

    added comments

--- messages/lab.js
+++ messages/lab.js
@@ -1,16 +1,22 @@
 function parse() 
 {
+	//create XMLHttpRequest instance
 	request = new XMLHttpRequest();
+	//get data, create HTTP request
 	request.open("GET", "data.json", true);
+	//manage data
 	request.onreadystatechange = parseData;
+	//execute request
 	request.send();
 }
 
 function parseData() 
 {
+	//request is ready and status code is successful
 	if (request.readyState == 4 && request.status == 200) {
 		result = document.getElementById("messages");
 		data = JSON.parse(request.responseText);
+		//place data in index.html
 		for (i = 0; i < data.length; i++) {
 			result.innerHTML += '<p class="input">' + data[i].content + ' <span id="name">' + data[i].username + "</span></p>";
 		}
