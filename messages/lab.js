e2fc3e5b3472ad0f92e99a70555f5bc841fe9772
Norman Young <nyyoung24@gmail.com>
2015-03-08 16:08:09 -0400

    debugging lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -9,11 +9,11 @@ function parse()
 function parseData() 
 {
 	if (request.readyState == 4 && request.status == 200) {
-		//result = document.getElementById("messages");
+		result = document.getElementById("messages");
 		data = JSON.parse(request.responseText);
 		for (i = 0; i < data.length; i++) {
-			result += "<p>" + data[i]["content"] + " - " + data[i]["username"] + "</p>";
+			result.innerHTML += "<p>" + data[i]["content"] + " - " + data[i]["username"] + "</p>";
 		}
-		document.getElementById("messages").innerHTML = request.responseText;
+		//document.getElementById("messages").innerHTML = request.responseText;
 	}
 }
